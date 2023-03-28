25da816b9e7557268144ee5dd33f4094be6c26a0
tetorgal <hector.galarza.css@gmail.com>
2023-03-27 20:46:53 -0600

    Nuevo Perfil

--- src/screens/Perfil.jsx
+++ src/screens/Perfil.jsx
@@ -3,102 +3,102 @@ import { View, Text, TextInput, StyleSheet, Button, TouchableOpacity } from 'rea
 import { FontAwesome } from '@expo/vector-icons';
 
 const Perfil = () => {
-
-    const [telefono, setTelefono] = useState('');
+    const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
-    const [fnacimiento, setFnacimiento] = useState('');
-    const [direccion, setDireccion] = useState('');
-    const [estadoCivil, setEstadocivil] = useState('');
-
+    const [address, setAddress] = useState('');
+  
     return (
-        <View>
-            <FontAwesome style={styles.user} name='user' size={80} color="#0A0821" />
-            <View style={styles.top}>
-                <Text style={[styles.topinfo, styles.top1]}>Nombre</Text>
-                <Text style={styles.topinfo}>Apellido</Text>
-                <Text style={[styles.topinfo, styles.top3]}>#Ruta</Text>
-            </View>
-
-            <View style={styles.datos}>
-                <TouchableOpacity>
-                    <FontAwesome style={styles.edit} name='edit' size={30} color="#fff" />
-                </TouchableOpacity>
-                <Text style={styles.label}>Teléfono:</Text>
-                <TextInput value={telefono} onChangeText={setTelefono} editable={false} style={styles.input}></TextInput>
-                <Text style={styles.label}>Correo:</Text>
-                <TextInput value={email} onChangeText={setEmail} editable={false} style={styles.input}></TextInput>
-                <Text style={styles.label}>Fecha de nacimiento:</Text>
-                <TextInput value={fnacimiento} onChangeText={setFnacimiento} editable={false} style={styles.input}></TextInput>
-                <Text style={styles.label}>Dirección:</Text>
-                <TextInput value={direccion} onChangeText={setDireccion} editable={false} style={styles.input}></TextInput>
-                <Text style={styles.label}>Estado Civil:</Text>
-                <TextInput value={estadoCivil} onChangeText={setEstadocivil} editable={false} style={styles.input}></TextInput>
-            </View>
+      <View style={styles.container}>
+        <View style={styles.banner}>
+          <View style={styles.userIcon}>
+            {/* insert user icon here */}
+          </View>
+          <View style={styles.userInfo}>
+            <Text style={styles.name}>John Smith</Text>
+            <Text style={styles.id}>ID: 123456</Text>
+          </View>
         </View>
-    )
-}
-
-const styles = StyleSheet.create({
-    user: {
-        position: 'absolute',
-        top: 25,
-        left: 10,
+        <View style={styles.form}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Phone:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter phone number"
+              value={phone}
+              onChangeText={setPhone}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Email:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter email address"
+              value={email}
+              onChangeText={setEmail}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Address:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter address"
+              value={address}
+              onChangeText={setAddress}
+            />
+          </View>
+        </View>
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
     },
-    edit: {
-        left: 295,
-        top: 10,
-        position: 'absolute'
+    banner: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 16,
+      borderBottomWidth: 1,
+      borderBottomColor: '#eee',
     },
-    datos: {
-        backgroundColor: '#0A0821',
-        height: 520,
-        width: 340,
-        borderRadius: 10,
-        marginTop: 40,
-        marginLeft: 10,
+    userIcon: {
+      width: 40,
+      height: 40,
+      borderRadius: 20,
+      backgroundColor: '#ccc',
+      marginRight: 16,
+    },
+    userInfo: {
+      flex: 1,
+    },
+    name: {
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    id: {
+      fontSize: 14,
+      color: '#666',
+    },
+    form: {
+      padding: 16,
+    },
+    inputContainer: {
+      marginBottom: 16,
     },
     label: {
-        fontSize: 20,
-        fontWeight: 700,
-        lineHeight: 30,
-        color: 'white',
-        marginTop: 20,
-        marginLeft: 15
+      fontSize: 16,
+      fontWeight: 'bold',
+      marginBottom: 8,
     },
     input: {
-        borderWidth: 1,
-        borderColor: '#D9D9D9',
-        backgroundColor: '#D9D9D9',
-        borderRadius: 5,
-        padding: 10,
-        width: '90%',
-        fontSize: 20,
-        color: 'black',
-        marginLeft: 15,
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: 8,
+      padding: 12,
+      fontSize: 16,
     },
-    top: {
-        position: 'relative',
-        backgroundColor: '#0A0821',
-        width: 271,
-        height: 80,
-        top: 20,
-        left: 80,
-        borderRadius: 10,
-    },
-    topinfo: {
-        color: 'white',
-        fontSize: 20,
-        padding: 10,
-    },
-    top1: {
-        fontWeight: 700,
-        marginBottom: -20
-    },
-    top3: {
-        fontWeight: 700,
-        marginTop: -60,
-        marginLeft: 160
-    },
-});
+  });
 
 export default Perfil
